b2c467fc0cc2acb317bba670b58cf5b82cfa5b9e
Adam Smith <adamcsmith98@gmail.com>
2025-06-05 13:56:20 +0100

    Add card for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,15 @@
 class BooksController < ApplicationController
+  require "net/http"
   def index
+    url = URI("https://www.googleapis.com/books/v1/volumes/s1gVAAAAYAAJ")
+    res = Net::HTTP.get(url)
+    parsed = JSON.parse(res)
+    @title = parsed["volumeInfo"]["title"]
+    @subtitle = parsed["volumeInfo"]["subtitle"]
+    @authors = parsed["volumeInfo"]["authors"]
+    @image_url = parsed["volumeInfo"]["imageLinks"]["thumbnail"]
+    @average_rating = parsed["volumeInfo"]["averageRating"]
+    @ratings_count = parsed["volumeInfo"]["ratingsCount"]
+    @description = parsed["volumeInfo"]["description"]
   end
 end
