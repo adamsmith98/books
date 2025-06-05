ef4091053b02aa4ae2477ee20e9ebca35d92bad9
Adam Smith <adamcsmith98@gmail.com>
2025-06-05 17:06:55 +0100

    Add multiple books to view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,20 @@
 class BooksController < ApplicationController
   require "net/http"
   def index
-    url = URI("https://www.googleapis.com/books/v1/volumes/s1gVAAAAYAAJ")
+    url = URI("https://www.googleapis.com/books/v1/volumes?q=pride+inauthor:austen&maxResults=40")
     res = Net::HTTP.get(url)
     parsed = JSON.parse(res)
-    @title = parsed["volumeInfo"]["title"]
-    @subtitle = parsed["volumeInfo"]["subtitle"]
-    @authors = parsed["volumeInfo"]["authors"]
-    @image_url = parsed["volumeInfo"]["imageLinks"]["thumbnail"]
-    @average_rating = parsed["volumeInfo"]["averageRating"]
-    @ratings_count = parsed["volumeInfo"]["ratingsCount"]
-    @description = parsed["volumeInfo"]["description"]
+    @books = []
+    for book in parsed["items"]
+      new_book = {}
+      new_book["title"] = book["volumeInfo"]["title"]
+      new_book["subtitle"] = book["volumeInfo"]["subtitle"]
+      new_book["authors"] = book["volumeInfo"]["authors"]
+      new_book["image_url"] = book["volumeInfo"]["imageLinks"]["thumbnail"] if book["volumeInfo"]["imageLinks"]
+      new_book["average_rating"] = book["volumeInfo"]["averageRating"]
+      new_book["ratings_count"] = book["volumeInfo"]["ratingsCount"]
+      new_book["description"] = book["volumeInfo"]["description"]
+      @books.append(new_book)
+    end
   end
 end
